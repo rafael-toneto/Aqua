9a93f668c378c833092b751453b1657ddb5f2ac2
rafael-toneto <rntoneto@icloud.com>
2026-07-20 16:00:11 -0300

    Initial Commit

---
+++ Aqua/AquaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AquaApp.swift
+//  Aqua
+//
+//  Created by Rafael Toneto on 20/07/26.
+//
+
+import SwiftUI
+
+@main
+struct AquaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
